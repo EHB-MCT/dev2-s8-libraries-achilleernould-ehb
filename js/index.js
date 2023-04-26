28ac9ff5d004290d889d2ad2a1d651358c537b47
Achille Ernould <achille.ernould@student.ehb.be>
2023-04-26 16:06:36 +0200

    popup

--- js/index.js
+++ js/index.js
@@ -1,25 +1,47 @@
 "use strict";
 
 const app = {
-    map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
-    init() {
-        // initialise de kaart
+	map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
+	init() {
+		// initialise de kaart
+		this.map = L.map("map").setView([50.846705, 4.352543], 15);
+		L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			maxZoom: 19,
+			attribution:
+				'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+		}).addTo(this.map);
+		// voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
+		// vergeet openstreetmap attributie niet
+		let marker = L.marker([50.846705, 4.352543]).addTo(this.map);
+		// gebruik de functie "loadMarkers" om de markers toe te voegen
+		this.loadMarkers();
+	},
+	loadMarkers() {
+		fetch(
+			"https://opendata.brussels.be/api/records/1.0/search/?dataset=toiletten&q=&rows=100&geofilter.distance=50.846475%2C+4.352793%2C+5000"
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
 
-        // voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-        // vergeet openstreetmap attributie niet
-        
-        // gebruik de functie "loadMarkers" om de markers toe te voegen
-    },
-    loadMarkers() {
+				for (let i = 0; i < data.records.length; i++) {
+					const lat = data.records[i].fields.wgs84_lat;
+					const lon = data.records[i].fields.wgs84_long;
 
-        // fetch de data van opendata.brussels.be
-            // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
-        
-        
-    },
-    addMarker(lat, lon) {
-        // voeg een marker toe op lat, lon
-    }
-}
+					this.addMarker(lat, lon);
+				}
+			});
+		// fetch de data van opendata.brussels.be
+		// als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
+	},
+	addMarker(lat, lon) {
+		// voeg een marker toe op lat, lon
+		let marker = L.marker([lat, lon]).addTo(this.map);
+
+		marker.addEventListener("click", function () {
+			this.marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+		});
+	},
+};
 
 app.init();
